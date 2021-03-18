e029aff9fb1d01f8e7395affb774107530b99593
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-03-18 02:43:42 -0700

    Trying another image fix

--- react-portfolio/src/components/Header/index.js
+++ react-portfolio/src/components/Header/index.js
@@ -7,7 +7,7 @@ function Header() {
         <nav className="navbar" role="navigation" aria-label="main navigation">
             <div className="navbar-brand">
                 <Link to={"/react-portfolio"} className="navbar-item logo">
-                    <img src="https://github.com/RyanKirkland86/react-portfolio/blob/main/react-portfolio/public/img/RKbrand.png" alt="R K initials logo"></img>
+                    <img src="./img/RKbrand.png" alt="R K initials logo"></img>
                 </Link>
             </div>
         </nav>
--- react-portfolio/src/pages/About.js
+++ react-portfolio/src/pages/About.js
@@ -20,7 +20,7 @@ function About() {
               <div className="tile is-ancestor">
                 <div className="tile is-4 is-vertical is-parent">
                   <div className="tile is-child box has-text-centered box">
-                    <img src="https://github.com/RyanKirkland86/react-portfolio/blob/main/react-portfolio/public/img/200%20Square%20Headshot.jpg" alt="Ryan Kirkland headshot" id="photo is-centered"></img>
+                    <img src="./img/200 Square Headshot.jpg" alt="Ryan Kirkland headshot" id="photo is-centered"></img>
                   </div>
                   <div className="tile is-child has-text-centered box">
                     <p className="subtitle"><strong>Education</strong></p>
