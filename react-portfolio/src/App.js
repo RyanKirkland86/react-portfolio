1c42983c3b394214d41e3005d13a58c0e84f84cb
Ryan Kirkland <ryan.j.kirkland86@gmail.com>
2021-03-17 21:47:10 -0700

    Adding more to App.js file and updating pages

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Work from "./pages/Work";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
+        <Footer />
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route exact path="/about" component={ About } />
---
+++ react-portfolio/src/pages/Work.js
@@ -0,0 +1,17 @@
+// import React, { useEffect, useState } from "react";
+import React from "react";
+import Project from "react";
+import projects from "../projects.json";
+
+function Work() {
+    const
+    return (
+        <div>
+            <Project
+                projects={projects}
+            />
+        </div>
+    )
+}
+
+export default Work;
